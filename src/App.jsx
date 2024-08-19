1f56bdd4e00fda5b9b380681c696efb6702e9e72
sakibhassan99 <sakibhassan.webdev@gmail.com>
2024-08-19 16:03:19 +0600

    Initial Setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import ExpenseForm from "./components/ExpenseForm";
+import ExpenseTable from "./components/ExpenseTable";
+
+function App() {
+  return (
+    <main>
+      <h1>Track Your Expense</h1>
+      <div className="expense-tracker">
+        <ExpenseForm />
+        <ExpenseTable />
+      </div>
+    </main>
+  );
+}
+
+export default App;
